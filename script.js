a684fdc31969ed7138dc662a4b690c0ad5e2ff3e
MPyscho <123724212+MPyscho@users.noreply.github.com>
2023-05-09 18:11:18 +0000

    hacer el script función 1

---
+++ script.js
@@ -0,0 +1,9 @@
+const numeroAdivinar = Math.floor(Math.random() * 15) + 1;
+
+let intentos = 0;
+
+function pedirNumero() {
+    const numero = prompt('Introduzca un número entre 1 y 15, tienes 3 intentos');
+    return parseInt(numero);
+}
+
