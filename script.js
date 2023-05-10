fa93206d658b6f88ebd2234f059d304c9a7297a0
MPyscho <123724212+MPyscho@users.noreply.github.com>
2023-05-10 20:01:50 +0200

    cambiado código entero con nuevas variables y condicones

--- script.js
+++ script.js
@@ -1,24 +1,58 @@
-const numeroAdivinar = Math.floor(Math.random() * 15) + 1;
+var numeroRandom = Math.floor(Math.random() * 15) + 1;
 
-let intentos = 3;
+console.log("Vamos a jugar a un juego, tienes que adivinar un número entre 1 y 15 en tres intentos.");
+console.log("Si fallas, te decimos si el número es mayor o menor y si aciertas, ganarás");
 
-function pedirNumero() {
-    const numero = prompt('Introduzca un número entre 1 y 15, tienes 3 intentos');
-    return parseInt(numero);
+let intentos = 0;
+
+function validarNumero(numero) {
+  var numeroNormal = Number(numero);
+
+  if (isNaN(numeroNormal)) {
+    console.log("El valor ingresado no es un número válido");
+    return false;
+  }
+
+  if (numeroNormal < 1 || numeroNormal > 15) {
+    console.log("El número debe estar entre 1 y 15");
+    return false;
+  }
+
+  return true;
+}
+function comprobarNumero(numero) {
+  intentos++;
+
+  if (!validarNumero(numero)) {
+    return false;
+  }
+  var numeroNormal = Number(numero);
+
+  if (numeroNormal === numeroRandom) {
+    console.log(`¡Felicidades! Has ganado el juego en ${intentos} intentos.`);
+    return true;
+  }
+
+  if (numeroNormal < numeroRandom) {
+    console.log("El número es mayor");
+  } else {
+    console.log("El número es menor");
+  }
+
+  return false;
 }
 
-function comprobarNumero(numero) {
-    intentos++;
+while (intentos < 3) {
+  var numeroIntroducido = prompt("Introduce un número");
 
-    if (numero === numeroAdivinar) {
-        alert(`Has acertado el número en ${intentos} intentos.`);
-    return true;
-  } else if (numero < numeroAdivinar) {
-    alert('El número es menor.');
-  } else {
-    alert('El número es mayor.');
+  if (comprobarNumero(numeroIntroducido)) {
+    break;
   }
-  if (intentos === 3 && numero != numeroAdivinar) {
-    alert(`Se acabaron tus intentos, el numero era ${ numeroAdivinar }.`);
+}
+
+if (intentos >= 3) {
+  console.log(`Has perdido el juego. El número era ${numeroRandom}.`);
+}
+
  
 
